3617891e550a57dace428a07ccf5a0c966221086
yorgyetson <yorgyetson@gmail.com>
2024-02-13 11:47:32 -0700

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,46 +1,53 @@
 <script lang="ts">
   import AddTask from "$lib/tasks/addTask.svelte"
+  import TaskTable from "$lib/tasks/taskTable.svelte"
   import { gql } from "@apollo/client/core"
-  import { onMount } from "svelte"
 
-  import { query } from "svelte-apollo"
-  // onMount(() => {
+  import { subscribe } from "svelte-apollo"
+
   const TASKS = gql`
-    query Tasks {
+    subscription Tasks {
       Tasks {
         id
+        name
+        description
+        created_at
+        complete
       }
     }
   `
 
-  const tasks = query(TASKS, {
+  const tasks = subscribe(TASKS, {
     variables: {},
   })
 
   $: console.log($tasks)
-  // })
 </script>
 
-<div class="navbar bg-base-300">
-  <div class="flex-1">
-    <a class="btn btn-ghost text-xl">daisyUI</a>
+<div class="flex w-full">
+  <ul class="menu p-4 w-1/4 h-screen min-h-full text-base-content">
+    <AddTask />
+  </ul>
+  <div class="overflow-x-auto m-2 w-full rounded p-2 shadow-xl">
+    <h1>Pending Tasks</h1>
+    {#if $tasks.loading}
+      <span class="loading loading-spinner loading-lg"></span>
+    {:else}
+      <TaskTable tasks={$tasks} />
+    {/if}
   </div>
-  <div class="flex-none">
-    <button class="btn btn-square btn-ghost">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        class="inline-block w-5 h-5 stroke-current"
-        ><path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-        ></path></svg
-      >
-    </button>
+  <div class="overflow-x-auto m-2 w-full rounded p-2 shadow-xl">
+    <h1>Finished Tasks</h1>
+    {#if $tasks.loading}
+      <span class="loading loading-spinner loading-lg"></span>
+    {:else}
+      <TaskTable tasks={$tasks} complete={true} />
+    {/if}
   </div>
 </div>
 
-<AddTask />
+<style>
+  h1 {
+    @apply text-2xl font-bold;
+  }
+</style>
